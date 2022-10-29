1c4088b3ee923726e3aa3a4fdf2923c450d7453d
ajikejay <77899606+ajikejay@users.noreply.github.com>
2022-10-29 14:19:44 +0100

    Update exoticanimals.sql

--- exoticanimals.sql
+++ exoticanimals.sql
@@ -22,3 +22,19 @@ INSERT INTO exoticanimals (Name, Orders, Country_Found)
    VALUES ("Pangolin", "Pholidota", "China");
    INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Fennec Fox", "Carnivora", "Arabia");
+   
+   SELECT * FROM exoticanimals;
+   
+   //*SELECT * from exoticanimals
+where country_found like "%Viet%"*//;
+
+SELECT * from exoticanimals
+where country_found = "Amazon";
+
+SELECT name, country_found from exoticanimals
+ORDER by orders;
+
+SELECT name, country_found from exoticanimals
+ORDER by orders
+LIMIT 2 OFFSET 2
+LIMIT 2;
