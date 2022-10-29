5270115de613ff13bcac55730315b9267a8de607
ajikejay <77899606+ajikejay@users.noreply.github.com>
2022-10-29 13:25:22 +0100

    Update exoticanimals.sql

--- exoticanimals.sql
+++ exoticanimals.sql
@@ -1,24 +1,24 @@
 CREATE TABLE exoticanimals (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     Name TEXT,
-    Order TEXT,
+    Orders TEXT,
     Country_Found TEXT
     
 ); 
 
-INSERT INTO exoticanimals (Name, Order, Country_Found) 
+INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Slow Loris", "Primate", "Vietnam");
-   INSERT INTO exoticanimals (Name, Order, Country_Found) 
+   INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Mandarin Duck", "Anseriformes", "China");
-   INSERT INTO exoticanimals (Name, Order, Country_Found) 
+   INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Tapir", "Perissodactyla", "Colombia");
-   INSERT INTO exoticanimals (Name, Order, Country_Found) 
+   INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Japanese Macaque", "Primate", "Japan");
-   INSERT INTO exoticanimals (Name, Order, Country_Found) 
+   INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Pink Dolphin", "Artiodactyla", "Amazon");
-   INSERT INTO exoticanimals (Name, Order, Country_Found) 
+   INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Atolepus Frog", "Atolepus Varius", "Costarica");
-   INSERT INTO exoticanimals (Name, Order, Country_Found) 
+   INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Pangolin", "Pholidota", "China");
-   INSERT INTO exoticanimals (Name, Order, Country_Found) 
+   INSERT INTO exoticanimals (Name, Orders, Country_Found) 
    VALUES ("Fennec Fox", "Carnivora", "Arabia");
